3d724d3214afdd450a1ab3c603791021ce00a0e5
Jiří Strnadel <Juzba88@gmail.com>
2025-02-25 00:10:09 +0100

    done?

--- src/components/Seznam.jsx
+++ src/components/Seznam.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Seznam.scss';
 import data from '../data';
 import OneCard from './OneCard';
 
 const Seznam = () => {
     const [text, setText] = useState('');
+    const [newData, setNewData] = useState(data);
+
+    useEffect(() => {
+        setNewData(
+            data.filter(({ title }) => {
+                return title.includes(text);
+            })
+        );
+    }, [text]);
 
     return (
         <div className='seznam'>
             <input type='text' value={text} onChange={(e) => setText(e.target.value)} placeholder='Hledaný film' />
             <div className='film-container'>
-                {data.map((data) => {
+                {newData.map((data) => {
                     return <OneCard key={data.id} {...data} />;
                 })}
             </div>
